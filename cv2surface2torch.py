d7831f24adeda7f8a13016aa18019b5335247509
xiang-zhe <36883691+xiang-zhe@users.noreply.github.com>
2021-09-27 13:01:34 +0800

    Create cv2surface2torch.py

---
+++ cv2surface2torch.py
@@ -0,0 +1,10 @@
+import torch
+import PyNvCodec as nvc
+
+def converter(im, nvUpl, surface_tensor, w, h, gpuID):
+    #h, w, c = im.shape
+    #nvUpl = nvc.PyFrameUploader(int(w), int(h), nvc.PixelFormat.RGB, gpuID)
+    #surface_tensor = torch.zeros(h, w, 3, dtype=torch.uint8, device=torch.device(f'cuda:{gpuID}'))
+    rawSurface = nvUpl.UploadSingleFrame(im)  #rawSurface.Format() == nvc.PixelFormat.RGB
+    rawSurface.PlanePtr().Export(surface_tensor.data_ptr(), w * 3, gpuID)  #surface_tensor.dtype == (torch.tensor, device = "cuda")
+    return surface_tensor
