3fe6cb89acddd0c29907f45e766ca500ea09fbca
xiang-zhe <36883691+xiang-zhe@users.noreply.github.com>
2021-09-29 10:37:09 +0800

    Update and rename cv2surface2torch.py to numpy2surface2torch.py

--- numpy2surface2torch.py
+++ numpy2surface2torch.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import PyNvCodec as nvc
 
-def converter2torch(rawFrame, nvUpl, surface_tensor, w, gpuID):
+def numpy2surface2torch(rawFrame, nvUpl, surface_tensor, w, gpuID):
     #h, w, c = rawFrame.shape
     #nvUpl = nvc.PyFrameUploader(int(w), int(h), nvc.PixelFormat.RGB, gpuID)
     #surface_tensor = torch.zeros(h, w, 3, dtype=torch.uint8, device=torch.device(f'cuda:{gpuID}'))
@@ -10,8 +10,11 @@ def converter2torch(rawFrame, nvUpl, surface_tensor, w, gpuID):
     rawSurface.PlanePtr().Export(surface_tensor.data_ptr(), w * 3, gpuID)  #surface_tensor.dtype == (torch.tensor, device = "cuda")
     return surface_tensor
 
-def converter2numpy(surface_tensor, nvDwn, rawFrame, gpuID):
-    #nvDwn = nvc.PySurfaceDownloader(hwidth, hheight, sureface_tensor.Format(), gpuID)
+def torch2surface2numpy(surface_tensor, nvDwn, rawFrame, w gpuID):
+    #h, w, c = surface_tensor.shape
+    #surface_rgb = nvc.Surface.Make(nvc.PixelFormat.RGB, w, h, gpuID)
+    #surface_rgb.PlanePtr().Import(surface_tensor.data_ptr(), w * 3, gpuID)
+    #nvDwn = nvc.PySurfaceDownloader(hwidth, hheight, nvc.PixelFormat.RGB, gpuID)
     #rawFrame = np.ndarray(shape=(), dtype=np.uint8)
     success = nvDwn.DownloadSingleSurface(surface_tensor, rawFrame)
     if success:
